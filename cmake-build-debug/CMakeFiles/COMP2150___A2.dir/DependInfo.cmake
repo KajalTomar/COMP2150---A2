2c6e72fe094e9937160db72f05c37283c2d7efe2
KajalTomar <kajaltomar09@gmail.com>
2021-03-07 12:57:16 -0600

    added the cmake file for Clion

---
+++ cmake-build-debug/CMakeFiles/COMP2150___A2.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/A2main.cpp" "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/cmake-build-debug/CMakeFiles/COMP2150___A2.dir/A2main.cpp.obj"
+  "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/Burst.cpp" "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/cmake-build-debug/CMakeFiles/COMP2150___A2.dir/Burst.cpp.obj"
+  "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/Event.cpp" "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/cmake-build-debug/CMakeFiles/COMP2150___A2.dir/Event.cpp.obj"
+  "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/ListItem.cpp" "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/cmake-build-debug/CMakeFiles/COMP2150___A2.dir/ListItem.cpp.obj"
+  "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/Node.cpp" "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/cmake-build-debug/CMakeFiles/COMP2150___A2.dir/Node.cpp.obj"
+  "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/ParentQueue.cpp" "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/cmake-build-debug/CMakeFiles/COMP2150___A2.dir/ParentQueue.cpp.obj"
+  "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/Process.cpp" "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/cmake-build-debug/CMakeFiles/COMP2150___A2.dir/Process.cpp.obj"
+  "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/Queue.cpp" "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/cmake-build-debug/CMakeFiles/COMP2150___A2.dir/Queue.cpp.obj"
+  "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/Simulation.cpp" "C:/Users/Kajal/Documents/COMP2150 - Object Orientation/COMP2150---A2/cmake-build-debug/CMakeFiles/COMP2150___A2.dir/Simulation.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/COMP2150___A2.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COMP2150___A2.dir/A2main.cpp.obj"
+  "CMakeFiles/COMP2150___A2.dir/Burst.cpp.obj"
+  "CMakeFiles/COMP2150___A2.dir/Event.cpp.obj"
+  "CMakeFiles/COMP2150___A2.dir/ListItem.cpp.obj"
+  "CMakeFiles/COMP2150___A2.dir/Node.cpp.obj"
+  "CMakeFiles/COMP2150___A2.dir/ParentQueue.cpp.obj"
+  "CMakeFiles/COMP2150___A2.dir/Process.cpp.obj"
+  "CMakeFiles/COMP2150___A2.dir/Queue.cpp.obj"
+  "CMakeFiles/COMP2150___A2.dir/Simulation.cpp.obj"
+  "COMP2150___A2.exe"
+  "COMP2150___A2.exe.manifest"
+  "COMP2150___A2.pdb"
+  "libCOMP2150___A2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COMP2150___A2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
