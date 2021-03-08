bca8e360e9a1af93d582b682a7ff840e6cc47e31
KajalTomar <kajaltomar09@gmail.com>
2021-03-08 14:44:29 -0600

    same thing, but on aviary

--- CatchTests.cpp
+++ CatchTests.cpp
@@ -3,7 +3,7 @@
 //#include "Catch2.h"
 #include "PriorityQueue.h"
 #include "Simulation.h"
-#include "ProcessArrival.h"
+#include "StartCPU.h"
 #include "Process.h"
 #include <iostream>
 using namespace std;
@@ -14,7 +14,7 @@ TEST_CASE("Testing enqueue edge cases"){
 	
 	Simulation * sim = new Simulation();
 	Process * newProcess = new Process("4 3 54 23 54",2);
-	ProcessArrival * testEvent = new ProcessArrival(4, newProcess, sim);
+	StartCPU* testEvent = new StartCPU(4, newProcess, sim);
 	
 	PriorityQueue * testQ = new PriorityQueue();
 	
@@ -26,7 +26,7 @@ TEST_CASE("Testing enqueue edge cases"){
 	
 	SECTION("Adding an item with lower priority (earlier in time) to a priority queue with exactly one item"){
 		Process * earliestProcess = new Process("1 2 4 5 -6 12",1);
-        ProcessArrival * earliestEvent = new ProcessArrival(2, earliestProcess, sim);
+        StartCPU* earliestEvent = new StartCPU(2, earliestProcess, sim);
 		
 		testQ->enqueue(earliestEvent); 
 
@@ -38,7 +38,7 @@ TEST_CASE("Testing enqueue edge cases"){
 	
 	SECTION("Adding a higgher priority item (later on in time) to a priority queue with exactly one item"){
 		Process * laterProcess = new Process("20 43 -4 1 -6",15);
-        ProcessArrival * laterEvent = new ProcessArrival(22, laterProcess, sim);
+        StartCPU* laterEvent = new StartCPU(22, laterProcess, sim);
 		
 		testQ->enqueue(laterEvent); 
 		
@@ -61,15 +61,15 @@ TEST_CASE("Testing enqueue general cases") {
     PriorityQueue * testQ = new PriorityQueue();
 
     SECTION("Adding a events in an already correct order of priorities") {
-        ProcessArrival * dequeued;
+        StartCPU* dequeued;
 
         cout << endl <<  endl << "TEST 4" << endl;
         for (int i = 0; i < 10; i++) {
-            testQ->enqueue(new ProcessArrival(i, new Process(("4 3 54 23 54"), i), sim));
+            testQ->enqueue(new StartCPU(i, new Process(("4 3 54 23 54"), i), sim));
         }
 
         while(testQ->getSize()>=2){
-            dequeued =  dynamic_cast<ProcessArrival *>(testQ->dequeue());
+            dequeued =  dynamic_cast<StartCPU*>(testQ->dequeue());
 
             dequeued->print();
 
@@ -81,14 +81,14 @@ TEST_CASE("Testing enqueue general cases") {
     } // SECTION 4
 
     SECTION("Adding a events in opposite of correct order of priorities. So each event should is of a higher priority that everything else on the list") {
-        ProcessArrival * dequeued;
+        StartCPU* dequeued;
         cout << endl <<  endl << "TEST 5" << endl;
         for (int i = 10; i > 0; i--) {
-            testQ->enqueue(new ProcessArrival(i, new Process(("4 3 54 23 54"), i), sim));
+            testQ->enqueue(new StartCPU(i, new Process(("4 3 54 23 54"), i), sim));
         }
 
         while(testQ->getSize()>=2){
-            dequeued =  dynamic_cast<ProcessArrival *>(testQ->dequeue());
+            dequeued =  dynamic_cast<StartCPU*>(testQ->dequeue());
 
             dequeued->print();
 
@@ -101,19 +101,19 @@ TEST_CASE("Testing enqueue general cases") {
     } // SECTION 5
 
     SECTION("Adding a events in mixed up priority") {
-        ProcessArrival * dequeued;
+        StartCPU* dequeued;
 
         cout << endl <<  endl << "TEST 4" << endl;
 
-        testQ->enqueue(new ProcessArrival(13, new Process(("4 3 -54 23 -54"), 0), sim));
-        testQ->enqueue(new ProcessArrival(54, new Process(("12 31 -4 3 -14"), 1), sim));
-        testQ->enqueue(new ProcessArrival(2, new Process(("13 3 -3 9 -11"), 2), sim));
-        testQ->enqueue(new ProcessArrival(12, new Process(("20 5"), 3), sim));
-        testQ->enqueue(new ProcessArrival(7, new Process(("21 44 -10"), 4), sim));
-        testQ->enqueue(new ProcessArrival(10, new Process(("25 4, -2, 12"), 5), sim));
+        testQ->enqueue(new StartCPU(13, new Process(("4 3 -54 23 -54"), 0), sim));
+        testQ->enqueue(new StartCPU(54, new Process(("12 31 -4 3 -14"), 1), sim));
+        testQ->enqueue(new StartCPU(2, new Process(("13 3 -3 9 -11"), 2), sim));
+        testQ->enqueue(new StartCPU(12, new Process(("20 5"), 3), sim));
+        testQ->enqueue(new StartCPU(7, new Process(("21 44 -10"), 4), sim));
+        testQ->enqueue(new StartCPU(10, new Process(("25 4, -2, 12"), 5), sim));
 
         while(testQ->getSize()>=2){
-            dequeued =  dynamic_cast<ProcessArrival *>(testQ->dequeue());
+            dequeued =  dynamic_cast<StartCPU*>(testQ->dequeue());
 
             dequeued->print();
 
