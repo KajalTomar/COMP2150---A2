ea22a70a5b6fc5ce4ea0a69fed310b73ee4c58cb
Kajal Tomar <tomark@grouse.cs.umanitoba.ca>
2021-03-13 21:14:09 -0600

    fixed a test to incorprate the new compare to methods based in if the event is a timeout or arrival event

--- CatchTests.cpp
+++ CatchTests.cpp
@@ -1,17 +1,13 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-//#include "Catch2.h"
 #include "PriorityQueue.h"
 #include "Simulation.h"
 #include "StartCPU.h"
+#include "ProcessArrival.h"
 #include "Process.h"
-#include <iostream>
 using namespace std;
 TEST_CASE("Testing enqueue edge cases"){
 
-	// CHANGE THESE TEST BECAUSE EVENT IS A VIRTUAL CLASS
-	// I JUST WANTED TO DO A QUICK TEST SO I MADE IT NON-VIRTUAL
-	
 	Simulation * sim = new Simulation();
 	Process * newProcess = new Process("4 3 54 23 54",2);
 	StartCPU* testEvent = new StartCPU(4, newProcess, sim);
@@ -26,14 +22,16 @@ TEST_CASE("Testing enqueue edge cases"){
 	
 	SECTION("Adding an item with higher priority (earlier in time) to a priority queue with exactly one item"){
 		Process * earliestProcess = new Process("1 2 4 5 -6 12",1);
-        StartCPU* earliestEvent = new StartCPU(2, earliestProcess, sim);
+        StartCPU* laterEvent = new StartCPU(2, earliestProcess, sim);
 		
-		testQ->enqueue(earliestEvent);
-		cout << "HERE" << endl;
-		cout << "New item: " << endl;
-        earliestEvent->print();
-        testQ->getFront()->print();
-		REQUIRE(earliestEvent->compareTo(testQ->getFront()) == 0);
+		testQ->enqueue(laterEvent);
+
+		ProcessArrival * earlierEvent = dynamic_cast<ProcessArrival * >(testQ->getFront());
+		
+		if(earlierEvent){
+			REQUIRE(earlierEvent->compareTo(testQ->getFront()) == 0);
+		}
+		REQUIRE(laterEvent->compareTo(testQ->getFront()) == -1);
 		REQUIRE(testQ->getSize() == 2);
 	
 	} // SECTION 2
@@ -66,15 +64,12 @@ TEST_CASE("Testing enqueue general cases") {
     SECTION("Adding a events in an already correct order of priorities") {
         StartCPU* dequeued;
 
-        cout << endl <<  endl << "TEST 4" << endl;
         for (int i = 0; i < 10; i++) {
             testQ->enqueue(new StartCPU(i, new Process(("4 3 54 23 54"), i), sim));
         }
 
         while(testQ->getSize()>=2){
             dequeued =  dynamic_cast<StartCPU*>(testQ->dequeue());
-
-            dequeued->print();
 
             if(dequeued != nullptr && dequeued->compareTo(testQ->getFront())){
                 REQUIRE(dequeued->compareTo(testQ->getFront()) <= 0);
@@ -85,15 +80,12 @@ TEST_CASE("Testing enqueue general cases") {
 
     SECTION("Adding a events in opposite of correct order of priorities. So each event should is of a higher priority that everything else on the list") {
         StartCPU* dequeued;
-        cout << endl <<  endl << "TEST 5" << endl;
         for (int i = 10; i > 0; i--) {
             testQ->enqueue(new StartCPU(i, new Process(("4 3 54 23 54"), i), sim));
         }
 
         while(testQ->getSize()>=2){
             dequeued =  dynamic_cast<StartCPU*>(testQ->dequeue());
-
-            dequeued->print();
 
             if(dequeued != nullptr && dequeued->compareTo(testQ->getFront())){
                 REQUIRE(dequeued->compareTo(testQ->getFront()) <= 0);
@@ -105,10 +97,8 @@ TEST_CASE("Testing enqueue general cases") {
 
     SECTION("Adding a events in mixed up priority") {
         StartCPU* dequeued;
-
-        cout << endl <<  endl << "TEST 4" << endl;
-
-        testQ->enqueue(new StartCPU(13, new Process(("4 3 -54 23 -54"), 0), sim));
+        
+		testQ->enqueue(new StartCPU(13, new Process(("4 3 -54 23 -54"), 0), sim));
         testQ->enqueue(new StartCPU(54, new Process(("12 31 -4 3 -14"), 1), sim));
         testQ->enqueue(new StartCPU(2, new Process(("13 3 -3 9 -11"), 2), sim));
         testQ->enqueue(new StartCPU(12, new Process(("20 5"), 3), sim));
@@ -117,8 +107,6 @@ TEST_CASE("Testing enqueue general cases") {
 
         while(testQ->getSize()>=2){
             dequeued =  dynamic_cast<StartCPU*>(testQ->dequeue());
-
-            dequeued->print();
 
             if(dequeued != nullptr && dequeued->compareTo(testQ->getFront())){
                 REQUIRE(dequeued->compareTo(testQ->getFront()) <= 0);
--- Makefile
+++ Makefile
@@ -5,8 +5,9 @@ objects = Event.o Simulation.o Burst.o ListItem.o Node.o ParentQueue.o PriorityQ
 # this rule will build A2 as the executable from the object files
 all: A2main.o $(objects)
 	$(CC) $(CFLAGS) -Wall -o A2 $< $(objects)  
-#all: CatchTests.o $(objects) 
-#	$(CC) $(CFLAGS) -Wall -o CatchTests $< $(objects) 
+
+Test: CatchTests.o $(objects) 
+	$(CC) $(CFLAGS) -Wall -o CatchTests $< $(objects) 
 
 
 #         # this rule will build a .o file from a .cpp file. 
